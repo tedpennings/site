ce6d054a16f353149b54ac3498f2077c8485442b
Ted Pennings <310323+tedpennings@users.noreply.github.com>
2017-12-05 16:08:26 -0800

    Update Bio.jsx

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -4,7 +4,7 @@ export default function () {
   return (
     <section className='bio mainColumnContent'>
       <p>
-        Ted lives in Portland, Oregon with his partner Jennie.
+        Ted lives in Portland, Oregon with his partner Jen.
       </p>
       <p>
         Ted works at <a href='http://www.invisionapp.com'>InVision</a> as
@@ -14,8 +14,8 @@ export default function () {
       <p>
         Ted has lived in a variety of places: southern California, Santa Cruz, Chicago,
         and the New Hampshire Seacoast. Ted and Jen spent six months 
-        <a href='https://gist.github.com/tedpennings/3f9eb98d6d153674beb4133c7c5326f4'>traveling 
-        around with world</a> with just two backpacks in early 2017.
+        traveling <a href='https://gist.github.com/tedpennings/3f9eb98d6d153674beb4133c7c5326f4'>around 
+        the world</a> with just two backpacks in early 2017.
       </p>
     </section>
   )
