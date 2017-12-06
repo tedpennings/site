6c545ec9ba87e0eeaf1b49e9b374bae011873742
Ted Pennings <310323+tedpennings@users.noreply.github.com>
2017-12-05 16:04:11 -0800

    Update Bio.jsx

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -4,16 +4,18 @@ export default function () {
   return (
     <section className='bio mainColumnContent'>
       <p>
-        Ted lives in Portland, Oregon with three cats. 🐈🐈🐈
+        Ted lives in Portland, Oregon with his partner Jennie.
       </p>
       <p>
-        By day, he works at <a href='http://www.invisionapp.com'>InVision</a> as
-        a software engineer. By night he enjoys all the delicious vegan food and
-        beer in the Rose City.
+        Ted works at <a href='http://www.invisionapp.com'>InVision</a> as
+        a software engineer. He enjoys travel, vegan desserts, yoga and skydiving.
+        He rarely posts here.
       </p>
       <p>
-        He's lived in a variety of places: southern California, Santa Cruz, Chicago,
-        and the New Hampshire Seacoast.
+        Ted has lived in a variety of places: southern California, Santa Cruz, Chicago,
+        and the New Hampshire Seacoast. Ted and Jen spent six months 
+        <a href='https://gist.github.com/tedpennings/3f9eb98d6d153674beb4133c7c5326f4'>traveling 
+        around with world</a> with just two backpacks in early 2017.
       </p>
     </section>
   )
