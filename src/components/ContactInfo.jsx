85c48f84364ca0d350362bd90688f75cc4b08838
Ted Pennings <310323+tedpennings@users.noreply.github.com>
2020-06-07 01:13:42 -0700

    2020 updates, rev 1

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -4,7 +4,7 @@ export default function () {
   return (
     <div className='linkArea'>
       <ul>
-        <li><a href='https://twitter.com/thesleepyvegan'>Twitter</a></li>
+        <li><a href='https://instagram.com/thesleepyvegan'>Instagram</a></li>
         <li><a href='https://github.com/tedpennings'>GitHub</a></li>
         <li><a href='https://www.linkedin.com/in/tedpennings'>LinkedIn</a></li>
       </ul>
