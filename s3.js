6822fe92001cb35107bf2d249ad62000a71e78c1
Ted Pennings <310323+tedpennings@users.noreply.github.com>
2020-08-23 16:58:13 -0700

    Remove s3 orb because it's slow

---
+++ s3.js
@@ -0,0 +1,34 @@
+const s3 = require("s3");
+
+// Shamelessly copied from s3 README
+// This is here because the Circle CI s3 orb takes 20s
+// to install awscli python package on every build
+
+const s3Options = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+};
+
+const client = s3.createClient({ s3Options });
+
+const params = {
+  localDir: "build",
+  deleteRemoved: true,
+
+  s3Params: {
+    Bucket: "ted.pennin.gs",
+  },
+};
+
+const uploader = client.uploadDir(params);
+
+uploader.on("error", function (err) {
+  console.error("unable to sync:", err.stack);
+});
+uploader.on("progress", function () {
+  console.log("progress", uploader.progressAmount, uploader.progressTotal);
+});
+uploader.on("end", function () {
+  console.log("done uploading");
+});
