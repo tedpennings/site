833c94b3120c0eb5dd6f95ce19a54c1c54fc20b0
Ted Pennings <310323+tedpennings@users.noreply.github.com>
2020-08-26 01:07:55 -0700

    Add Fade to lessen font loading jitter

--- src/sidebar.jsx
+++ src/sidebar.jsx
@@ -5,7 +5,8 @@ import { makeStyles } from "@material-ui/core/styles";
 
 import SidebarContent from "./sidebar_content.mdx";
 
-import me2020 from "./assets/me-2020.jpg";
+// Force image into bundle to avoid layout twitch
+import me2020 from "!!url-loader?limit=409029395329!./assets/me-2020.jpg";
 
 const useStyles = makeStyles((theme) => ({
   photo: {
