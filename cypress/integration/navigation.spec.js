74ec3970495a0cdc8300dd57fc3d4622dffc44cc
Ted Pennings <310323+tedpennings@users.noreply.github.com>
2020-09-05 20:07:23 -0700

    Another approach for the tests

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -11,13 +11,21 @@ context("Navigation", () => {
 
   it("navigates to a page from the body links", () => {
     cy.get("[data-link-name=Heatmaps]").click();
-    cy.get("h2").contains("Heatmaps").should("be.visible");
+    cy.url().should("contain", "/dataviz/heatmaps");
+    cy.contains("h2", "Heatmaps");
   });
 
   it("updates the breadcrumbs", () => {
+    cy.getByTestId("breadcrumb").should("have.length", 1);
+
     cy.get("[data-link-name=Heatmaps]").click();
-    cy.getByTestId("breadcrumb")
-      .filter("[data-breadcrumb-level=2][data-breadcrumb-name=Heatmaps]")
-      .should("be.visible");
+    cy.contains("[data-breadcrumb-level=0]", "Home");
+    cy.contains("[data-breadcrumb-level=1]", "Data Visualization");
+    cy.contains("[data-breadcrumb-level=2]", "Heatmaps");
+    cy.getByTestId("breadcrumb").should("have.length", 3);
+
+    cy.contains("[data-breadcrumb-level=1]", "Data Visualization").click();
+    cy.get("[data-breadcrumb-level=2]").should("not.exist"); // Heatmaps is gone
+    cy.getByTestId("breadcrumb").should("have.length", 2);
   });
 });
