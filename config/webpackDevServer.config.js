d6b82564e1e72feafd304435a9512820032208a7
Ted Pennings <310323+tedpennings@users.noreply.github.com>
2020-09-05 21:00:14 -0700

    Misc dependency upgrades (#68)

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -5,7 +5,6 @@ const noopServiceWorkerMiddleware = require("react-dev-utils/noopServiceWorkerMi
 const ignoredFiles = require("react-dev-utils/ignoredFiles");
 const redirectServedPath = require("react-dev-utils/redirectServedPathMiddleware");
 const paths = require("./paths");
-const getHttpsConfig = require("./getHttpsConfig");
 
 const host = process.env.HOST || "0.0.0.0";
 const sockHost = process.env.WDS_SOCKET_HOST;
@@ -88,7 +87,6 @@ module.exports = function (proxy, allowedHost) {
     watchOptions: {
       ignored: ignoredFiles(paths.appSrc),
     },
-    https: getHttpsConfig(),
     host,
     overlay: false,
     historyApiFallback: {
