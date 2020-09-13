5b9ae0045287d99bee2abf9128710d55360098a2
Ted Pennings <310323+tedpennings@users.noreply.github.com>
2020-09-12 18:46:21 -0700

    Working with jsx-as-text

--- src/common/mdx_rendering.test.js
+++ src/common/mdx_rendering.test.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { renderWithReact } from "@mdx-js/test-util";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
 import mdxComponents from "./mdx_components";
 
 describe("MDX rendering", () => {
@@ -7,9 +10,12 @@ describe("MDX rendering", () => {
     const jsx = await renderWithReact(
       `# hello
 
-    `,
+      `,
       { components: mdxComponents }
     );
-    expect(jsx).toEqual(true);
+    render(jsx);
+    expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
+      "hello"
+    );
   });
 });
