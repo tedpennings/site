a06cb9504ffe8a280366cfc5ba7e58d4da739edc
Ted Pennings <310323+tedpennings@users.noreply.github.com>
2020-09-12 19:11:41 -0700

    h1, h2 tests

--- src/common/mdx_rendering.test.js
+++ src/common/mdx_rendering.test.js
@@ -1,21 +1,39 @@
-import React from "react";
 import { renderWithReact } from "@mdx-js/test-util";
-import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+import { screen } from "@testing-library/dom";
 import "@testing-library/jest-dom/extend-expect";
 
 import mdxComponents from "./mdx_components";
 
-describe("MDX rendering", () => {
-  it("renders h1 tags correctly", async () => {
-    const jsx = await renderWithReact(
-      `# hello
+async function renderMarkdown(markdown) {
+  const html = await renderWithReact(markdown, { components: mdxComponents });
+  document.body.innerHTML = html;
+}
 
-      `,
-      { components: mdxComponents }
-    );
-    render(jsx);
-    expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
-      "hello"
-    );
+describe("MDX rendering", () => {
+  describe("headings", () => {
+    it("renders h1 tags correctly", async () => {
+      await renderMarkdown(headingMarkdown(1));
+      expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
+        "heading one"
+      );
+      expect(screen.getByRole("heading", { level: 1 })).toHaveClass(
+        "MuiTypography-h1"
+      );
+    });
+
+    it("renders h2 tags correctly", async () => {
+      await renderMarkdown(headingMarkdown(2));
+      expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
+        "heading two"
+      );
+      expect(screen.getByRole("heading", { level: 2 })).toHaveClass(
+        "MuiTypography-h2"
+      );
+    });
   });
 });
+
+const headingWords = [null, "one", "two", "three", "four", "five", "six"];
+const hashes = (count) => new Array(count).fill("#").join("");
+const headingMarkdown = (level) =>
+  [hashes(level), "heading", headingWords[level], "\n\n"].join(" ");
