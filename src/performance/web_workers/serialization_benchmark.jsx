8d92e4121b8940d56903c840777e19644b05ca14
Ted Pennings <310323+tedpennings@users.noreply.github.com>
2020-11-11 01:13:38 -0800

    wip

--- src/performance/web_workers/serialization_benchmark.jsx
+++ src/performance/web_workers/serialization_benchmark.jsx
@@ -1,10 +1,10 @@
 import React, { useCallback, useEffect, useMemo, useRef } from "react";
-import { useRafState } from "react-use";
-import { Box, CircularProgress, Typography } from "@material-ui/core";
+import { useMeasure, useRafState } from "react-use";
+import { Box, LinearProgress, Typography } from "@material-ui/core";
 import { useTheme } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import { scaleLinear } from "d3-scale";
-import { axisBottom, axisLeft } from "d3-axis";
-import { selection } from "d3-selection";
+import sha512 from "hash.js/lib/hash/sha/512";
 
 import NoopWorker from "worker-loader!./noop_worker.js";
 
@@ -14,9 +14,30 @@ import NoopWorker from "worker-loader!./noop_worker.js";
 import CovidDatasetObject from "../../datasets/covid.json";
 const CovidDatasetJsonText = JSON.stringify(CovidDatasetObject);
 
-const DEFAULT_UNIT_SIZE = 25;
-const SAMPLE_COUNT = 25;
-const TOTAL_RUN_COUNT = DEFAULT_UNIT_SIZE * SAMPLE_COUNT;
+// Series keys (mostly used to assign colors)
+const COVID_JSON = "covid_json";
+const COVID_OBJ = "covid_obj";
+const SMALL_RANDOM_JSON = "small_random_json";
+const SMALL_RANDOM_OBJ = "small_random_obj";
+const LARGE_RANDOM_JSON = "large_random_json";
+const LARGE_RANDOM_OBJ = "large_random_obj";
+const SERIES_KEYS = [
+  COVID_JSON,
+  COVID_OBJ,
+  SMALL_RANDOM_JSON,
+  SMALL_RANDOM_OBJ,
+  LARGE_RANDOM_JSON,
+  LARGE_RANDOM_OBJ,
+];
+
+const DEFAULT_UNIT_SIZE = 15;
+const DEFAULT_BENCHMARK_RUNS = 100;
+const TOTAL_SAMPLES =
+  DEFAULT_UNIT_SIZE * DEFAULT_BENCHMARK_RUNS * SERIES_KEYS.length;
+
+const X_AXIS_HEIGHT = 24;
+const Y_AXIS_WIDTH = 54;
+const CANVAS_BUFFER = 10;
 
 const noopWorker = new NoopWorker();
 
@@ -31,48 +52,93 @@ function benchmarkPostMessage(testSubject) {
   return timings;
 }
 
+function randomData(n = 10) {
+  return Array(n).fill(1).map(Math.random).map(sha512);
+}
+
+function createComplexNewMessage(count) {
+  count = count ?? Math.round(Math.random() * 10000);
+  const arr = Array(count).fill(1).map(Math.random);
+
+  for (let i = 0; i < count / 2; i++) {
+    const idx = (i * Math.random * arr.length) | 1;
+    arr[idx] = randomData();
+  }
+
+  return arr;
+}
+
+const useStyles = makeStyles((theme) => ({
+  axisBackground: {
+    fill: theme.palette.primary.main,
+    fillOpacity: 0.1,
+  },
+  axisTickText: {
+    ...theme.typography.subtitle1,
+    fontSize: "0.75rem",
+    fill: theme.palette.primary.main,
+    pointerEvents: "none",
+    userSelect: "none",
+  },
+  sampleCountText: {
+    ...theme.typography.subtitle1,
+    fontSize: "0.75rem",
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+}));
+
 function useScales({ width, height, xDomain, yDomain }) {
   return useMemo(() => {
     const xScale = scaleLinear()
-      .domain([xDomain[0], xDomain[1]])
-      .range([10, width - 10])
+      .domain(xDomain)
+      .range([CANVAS_BUFFER, width - Y_AXIS_WIDTH - CANVAS_BUFFER])
       .nice();
-    const yScale = scaleLinear()
-      .domain([yDomain[0], yDomain[1]])
-      .range([height - 10, 10])
-      .nice();
+    xScale.clamp();
+    const xTicks = xScale.ticks(6);
 
-    return { xScale, yScale };
+    const yScale = scaleLinear()
+      .domain(yDomain)
+      .range([height - X_AXIS_HEIGHT - CANVAS_BUFFER, CANVAS_BUFFER])
+      .nice();
+    yScale.clamp();
+    const yTicks = yScale.ticks(4);
+
+    return { xScale, xTicks, yScale, yTicks };
   }, [width, height, xDomain, yDomain]);
 }
 
 export default function SerializationBenchmark() {
   const theme = useTheme();
+  const classes = useStyles();
+  const [ref, { height, width }] = useMeasure();
+
+  // TODO this hook should handle scales and just take data as input
   const [{ xDomain, yDomain }, setDomains] = useRafState({
     xDomain: [],
     yDomain: [],
   });
-  const results = useRef({ covidObject: [], covidJson: [] });
+  const results = useRef([]);
+  const startTime = useRef(window.performance.now());
 
   const calculateDomains = useCallback(() => {
     let xDomainMin = Number.MAX_SAFE_INTEGER;
     let xDomainMax = Number.MIN_SAFE_INTEGER;
     let yDomainMin = Number.MAX_SAFE_INTEGER;
     let yDomainMax = Number.MIN_SAFE_INTEGER;
-    [...results.current.covidObject, ...results.current.covidJson].forEach(
-      ([timestamp, duration]) => {
-        if (timestamp < xDomainMin) {
-          xDomainMin = timestamp;
-        } else if (timestamp > xDomainMax) {
-          xDomainMax = timestamp;
-        }
-        if (duration < yDomainMin) {
-          yDomainMin = duration;
-        } else if (duration > yDomainMax) {
-          yDomainMax = duration;
-        }
+
+    results.current.forEach(({ timestamp, duration }) => {
+      if (timestamp < xDomainMin) {
+        xDomainMin = timestamp;
+      } else if (timestamp > xDomainMax) {
+        xDomainMax = timestamp;
       }
-    );
+      if (duration < yDomainMin) {
+        yDomainMin = duration;
+      } else if (duration > yDomainMax) {
+        yDomainMax = duration;
+      }
+    });
 
     setDomains({
       xDomain: [xDomainMin, xDomainMax],
@@ -82,13 +148,17 @@ export default function SerializationBenchmark() {
 
   const afterBenchmark = useCallback(
     (timings, name) => {
-      results.current[name].push(...timings);
+      timings.forEach((t) =>
+        results.current.push({
+          duration: t[1],
+          timestamp: t[0],
+          name,
+        })
+      );
       if (
-        results.current[name].length === DEFAULT_UNIT_SIZE * SAMPLE_COUNT ||
-        results.current[name].length % 20 === 0
+        results.current.length % 50 === 0 ||
+        results.current.length === TOTAL_SAMPLES
       ) {
-        // render progress on scatterplot
-        // setting domains triggers render
         calculateDomains();
       }
     },
@@ -96,75 +166,162 @@ export default function SerializationBenchmark() {
   );
 
   useEffect(() => {
-    for (let i = 0; i < SAMPLE_COUNT; i++) {
+    const smallerMessage = createComplexNewMessage(300);
+    const smallerMessageJson = JSON.stringify(smallerMessage);
+    const biggerMessage = createComplexNewMessage(15000);
+    const biggerMessageJson = JSON.stringify(biggerMessage);
+    for (let i = 0; i < DEFAULT_BENCHMARK_RUNS; i++) {
       window.requestIdleCallback(() => {
-        afterBenchmark(benchmarkPostMessage(CovidDatasetObject), "covidObject");
-        afterBenchmark(benchmarkPostMessage(CovidDatasetJsonText), "covidJson");
+        afterBenchmark(benchmarkPostMessage(CovidDatasetObject), COVID_OBJ);
+        afterBenchmark(benchmarkPostMessage(CovidDatasetJsonText), COVID_JSON);
+      });
+
+      window.requestIdleCallback(() => {
+        afterBenchmark(benchmarkPostMessage(smallerMessage), SMALL_RANDOM_OBJ);
+        afterBenchmark(
+          benchmarkPostMessage(smallerMessageJson),
+          SMALL_RANDOM_JSON
+        );
+        afterBenchmark(benchmarkPostMessage(biggerMessage), LARGE_RANDOM_OBJ);
+        afterBenchmark(
+          benchmarkPostMessage(biggerMessageJson),
+          LARGE_RANDOM_JSON
+        );
       });
     }
   }, [afterBenchmark]);
 
-  const width = 1000;
-  const height = 400;
-
-  const { xScale, yScale } = useScales({ width, height, xDomain, yDomain });
-
-  function renderAxes(el) {
-    // TODO this is busted. I think I need to write my own axes.
-    const xAxis = axisBottom(xScale).ticks(6);
-    selection(el).enter().append("g").call(xAxis);
-    const yAxis = axisLeft(yScale).ticks(4);
-    selection(el).enter().append("g").call(yAxis);
-  }
+  const { xScale, xTicks, yScale, yTicks } = useScales({
+    width,
+    height,
+    xDomain,
+    yDomain,
+  });
 
   return (
     <Box>
-      <svg
-        width={width}
-        height={height}
-        viewBox={`0 0 ${width} ${height}`}
-        ref={renderAxes}
+      <Box
+        ref={ref}
+        display="flex"
+        flexDirection="column"
+        flex="1 1 100%"
+        height={480}
+        width="100%"
       >
-        {results.current.covidObject.map(([timestamp, duration]) => (
-          <circle
-            key={`object-${timestamp}`}
-            r={2.5}
-            cx={xScale(timestamp)}
-            cy={yScale(duration)}
-            fill={theme.palette.series[0]}
-          />
-        ))}
-        {results.current.covidJson.map(([timestamp, duration], i) => (
-          <circle
-            key={`json-${i}`}
-            r={2.5}
-            cx={xScale(timestamp)}
-            cy={yScale(duration)}
-            fill={theme.palette.series[1]}
-          />
-        ))}
-      </svg>
-      <Box display="flex">
-        <Box
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          bgcolor="grey.100"
-          borderRadius="borderRadius"
-          padding={1}
-          width={120}
-        >
-          <CircularProgress
-            variant="static"
-            value={(results.current.covidObject.length / TOTAL_RUN_COUNT) * 100}
-          />
-          <Typography variant="subtitle" component="div" marginTop={1}>
-            {results.current.covidObject.length === TOTAL_RUN_COUNT
-              ? `${TOTAL_RUN_COUNT} samples`
-              : `${results.current.covidObject.length} / ${TOTAL_RUN_COUNT}`}
-          </Typography>
-        </Box>
+        <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
+          <g data-x-axis>
+            <rect
+              className={classes.axisBackground}
+              x={0}
+              y={height - X_AXIS_HEIGHT}
+              width={width}
+              height={X_AXIS_HEIGHT}
+              fill={theme.palette.primary.main}
+              fillOpacity={0.05}
+            />
+            {xTicks
+              .filter((t) => xScale(t) > Y_AXIS_WIDTH * 1.25)
+              .map((t, idx) => (
+                <React.Fragment key={`x-tick-${idx}`}>
+                  <line
+                    key={t}
+                    x1={xScale(t)}
+                    x2={xScale(t)}
+                    y1={height - X_AXIS_HEIGHT}
+                    y2={height}
+                    stroke={theme.palette.primary.main}
+                    strokeWidth={2}
+                  />
+                  <text
+                    className={classes.axisTickText}
+                    x={xScale(t) + 4}
+                    y={height - X_AXIS_HEIGHT + 16}
+                  >
+                    {new Date(
+                      window.performance.timeOrigin + t
+                    ).toLocaleTimeString()}
+                  </text>
+                </React.Fragment>
+              ))}
+          </g>
+          <g data-y-axis>
+            <rect
+              className={classes.axisBackground}
+              x={0}
+              y={0}
+              width={Y_AXIS_WIDTH}
+              height={height - X_AXIS_HEIGHT}
+            />
+            {yTicks
+              .filter((t) => yScale(t) > X_AXIS_HEIGHT * 1.25)
+              .map((t, idx) => (
+                <React.Fragment key={`y-tick-${idx}`}>
+                  <line
+                    key={t}
+                    y1={yScale(t)}
+                    y2={yScale(t)}
+                    x1={0}
+                    x2={Y_AXIS_WIDTH}
+                    stroke={theme.palette.primary.main}
+                    strokeWidth={2}
+                  />
+
+                  <text
+                    className={classes.axisTickText}
+                    x={2}
+                    y={yScale(t) - 4}
+                    textAnchor="start"
+                  >
+                    {t} ms
+                  </text>
+                </React.Fragment>
+              ))}
+          </g>
+          <g data-results>
+            {results.current.map(({ name, timestamp, duration }) => (
+              <circle
+                className={classes[name]}
+                key={`result-${name}-${timestamp}`}
+                r={2.5}
+                fill={theme.palette.series[SERIES_KEYS.indexOf(name)]}
+                cx={xScale(timestamp)}
+                cy={yScale(duration)}
+              />
+            ))}
+          </g>
+        </svg>
+      </Box>
+      <Box mt={2} mb={2}>
+        <Typography className={classes.sampleCountText} component="div">
+          {results.current.length.toLocaleString()} samples{" "}
+          {results.current.length === TOTAL_SAMPLES
+            ? `(${formatTime(
+                results.current[results.current.length - 1].timestamp -
+                  startTime.current
+              )})`
+            : `(${Math.round(
+                (results.current.length / TOTAL_SAMPLES) * 100
+              )}%)`}
+        </Typography>
+        <LinearProgress
+          variant="determinate"
+          value={(results.current.length / TOTAL_SAMPLES) * 100}
+        />
       </Box>
     </Box>
   );
+}
+
+function formatTime(millis) {
+  if (millis > 90 * 1000) {
+    // 90 seconds or longer
+    const mins = Math.round(millis / 1000 / 60);
+    const sec = Math.round((millis / 1000) % 60);
+    return `${mins}m ${sec}s`;
+  }
+  if (millis > 4 * 1000) {
+    // 4 seconds or longer
+    return `${Math.round(millis / 1000)}s`;
+  }
+  return `${Math.round(millis)}ms`;
 }
