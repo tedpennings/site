4b170db47b26555808b7ffcac78e69f147d535f2
Ted Pennings <310323+tedpennings@users.noreply.github.com>
2020-11-11 17:33:06 -0800

    Extract scatterplot

---
+++ src/common/scatter_plot.jsx
@@ -0,0 +1,191 @@
+import React, { useMemo } from "react";
+import PropTypes from "prop-types";
+import { useMeasure } from "react-use";
+import { Box } from "@material-ui/core";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { scaleLinear } from "d3-scale";
+
+const X_AXIS_HEIGHT = 24;
+const Y_AXIS_WIDTH = 54;
+const CANVAS_BUFFER = 10;
+
+const useStyles = makeStyles((theme) => ({
+  axisBackground: {
+    fill: theme.palette.primary.main,
+    fillOpacity: 0.1,
+  },
+  axisTickText: {
+    ...theme.typography.subtitle1,
+    fontSize: "0.75rem",
+    fill: theme.palette.primary.main,
+    pointerEvents: "none",
+    userSelect: "none",
+  },
+}));
+
+export default function ScatterPlot({ points, pointColors, pointTitles }) {
+  const theme = useTheme();
+  const classes = useStyles();
+  const [ref, { height, width }] = useMeasure();
+
+  const { xScale, xTicks, yScale, yTicks } = useScales({
+    points,
+    height,
+    width,
+  });
+
+  return (
+    <Box
+      ref={ref}
+      display="flex"
+      flexDirection="column"
+      flex="1 1 100%"
+      height={480}
+      width="100%"
+    >
+      <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
+        <g data-x-axis>
+          {height + width > 0 && (
+            <rect
+              className={classes.axisBackground}
+              x={0}
+              y={height - X_AXIS_HEIGHT}
+              width={width}
+              height={X_AXIS_HEIGHT}
+              fill={theme.palette.primary.main}
+              fillOpacity={0.05}
+            />
+          )}
+          {xTicks
+            .filter((t) => xScale(t) > Y_AXIS_WIDTH * 1.25)
+            .map((t, idx) => (
+              <React.Fragment key={`x-tick-${idx}`}>
+                <line
+                  key={t}
+                  x1={xScale(t)}
+                  x2={xScale(t)}
+                  y1={height - X_AXIS_HEIGHT}
+                  y2={height}
+                  stroke={theme.palette.primary.main}
+                  strokeWidth={2}
+                />
+                <text
+                  className={classes.axisTickText}
+                  x={xScale(t) + 4}
+                  y={height - X_AXIS_HEIGHT + 16}
+                >
+                  {new Date(
+                    window.performance.timeOrigin + t
+                  ).toLocaleTimeString()}
+                </text>
+              </React.Fragment>
+            ))}
+        </g>
+        <g data-y-axis>
+          {height > 0 && (
+            <rect
+              className={classes.axisBackground}
+              x={0}
+              y={0}
+              width={Y_AXIS_WIDTH}
+              height={height - X_AXIS_HEIGHT}
+            />
+          )}
+          {yTicks
+            .filter((t) => yScale(t) > X_AXIS_HEIGHT * 1.25)
+            .map((t, idx) => (
+              <React.Fragment key={`y-tick-${idx}`}>
+                <line
+                  key={t}
+                  y1={yScale(t)}
+                  y2={yScale(t)}
+                  x1={0}
+                  x2={Y_AXIS_WIDTH}
+                  stroke={theme.palette.primary.main}
+                  strokeWidth={2}
+                />
+
+                <text
+                  className={classes.axisTickText}
+                  x={2}
+                  y={yScale(t) - 4}
+                  textAnchor="start"
+                >
+                  {t} ms
+                </text>
+              </React.Fragment>
+            ))}
+        </g>
+        <g data-results>
+          {points.map(({ name, x, y }, idx) => (
+            <circle
+              className={classes[name]}
+              key={`point-${x}-${y}-${idx}`}
+              r={2.5}
+              fill={pointColors[name]}
+              cx={xScale(x)}
+              cy={yScale(y)}
+            />
+          ))}
+        </g>
+      </svg>
+    </Box>
+  );
+}
+ScatterPlot.propTypes = {
+  points: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      x: PropTypes.number,
+      y: PropTypes.number,
+    })
+  ),
+  pointColors: PropTypes.objectOf(PropTypes.string),
+  pointTitles: PropTypes.objectOf(PropTypes.string),
+};
+
+function useScales({ width, height, points }) {
+  const { xDomain, yDomain } = useDomains({ points });
+  return useMemo(() => {
+    const xScale = scaleLinear()
+      .domain(xDomain)
+      .range([CANVAS_BUFFER, width - Y_AXIS_WIDTH - CANVAS_BUFFER])
+      .nice();
+    xScale.clamp();
+    const xTicks = xScale.ticks(6);
+
+    const yScale = scaleLinear()
+      .domain(yDomain)
+      .range([height - X_AXIS_HEIGHT - CANVAS_BUFFER, CANVAS_BUFFER])
+      .nice();
+    yScale.clamp();
+    const yTicks = yScale.ticks(4);
+
+    return { xScale, xTicks, yScale, yTicks };
+  }, [width, height, xDomain, yDomain]);
+}
+
+function useDomains({ points }) {
+  let xDomainMin = Number.MAX_SAFE_INTEGER;
+  let xDomainMax = Number.MIN_SAFE_INTEGER;
+  let yDomainMin = Number.MAX_SAFE_INTEGER;
+  let yDomainMax = Number.MIN_SAFE_INTEGER;
+
+  points.forEach(({ x, y }) => {
+    if (x < xDomainMin) {
+      xDomainMin = x;
+    } else if (x > xDomainMax) {
+      xDomainMax = x;
+    }
+    if (y < yDomainMin) {
+      yDomainMin = y;
+    } else if (y > yDomainMax) {
+      yDomainMax = y;
+    }
+  });
+
+  return {
+    xDomain: [xDomainMin, xDomainMax],
+    yDomain: [yDomainMin, yDomainMax],
+  };
+}
--- src/performance/web_workers/serialization_benchmark.jsx
+++ src/performance/web_workers/serialization_benchmark.jsx
@@ -1,10 +1,15 @@
-import React, { useCallback, useEffect, useMemo, useRef } from "react";
-import { useMeasure, useRafState } from "react-use";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { Box, LinearProgress, Typography } from "@material-ui/core";
-import { useTheme } from "@material-ui/core/styles";
-import { makeStyles } from "@material-ui/core/styles";
-import { scaleLinear } from "d3-scale";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import sha512 from "hash.js/lib/hash/sha/512";
+
+import { formatTime } from "../../common/human";
 
 import NoopWorker from "worker-loader!./noop_worker.js";
 
@@ -12,6 +17,7 @@ import NoopWorker from "worker-loader!./noop_worker.js";
 // const echoWorker = new EchoWorker();
 
 import CovidDatasetObject from "../../datasets/covid.json";
+import ScatterPlot from "../../common/scatter_plot";
 const CovidDatasetJsonText = JSON.stringify(CovidDatasetObject);
 
 // Series keys (mostly used to assign colors)
@@ -35,10 +41,14 @@ const DEFAULT_BENCHMARK_RUNS = 100;
 const TOTAL_SAMPLES =
   DEFAULT_UNIT_SIZE * DEFAULT_BENCHMARK_RUNS * SERIES_KEYS.length;
 
-const X_AXIS_HEIGHT = 24;
-const Y_AXIS_WIDTH = 54;
-const CANVAS_BUFFER = 10;
-
+const useStyles = makeStyles((theme) => ({
+  sampleCountText: {
+    ...theme.typography.subtitle1,
+    fontSize: "0.75rem",
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+}));
 const noopWorker = new NoopWorker();
 
 function benchmarkPostMessage(testSubject) {
@@ -68,102 +78,27 @@ function createComplexNewMessage(count) {
   return arr;
 }
 
-const useStyles = makeStyles((theme) => ({
-  axisBackground: {
-    fill: theme.palette.primary.main,
-    fillOpacity: 0.1,
-  },
-  axisTickText: {
-    ...theme.typography.subtitle1,
-    fontSize: "0.75rem",
-    fill: theme.palette.primary.main,
-    pointerEvents: "none",
-    userSelect: "none",
-  },
-  sampleCountText: {
-    ...theme.typography.subtitle1,
-    fontSize: "0.75rem",
-    display: "flex",
-    justifyContent: "flex-end",
-  },
-}));
-
-function useScales({ width, height, xDomain, yDomain }) {
-  return useMemo(() => {
-    const xScale = scaleLinear()
-      .domain(xDomain)
-      .range([CANVAS_BUFFER, width - Y_AXIS_WIDTH - CANVAS_BUFFER])
-      .nice();
-    xScale.clamp();
-    const xTicks = xScale.ticks(6);
-
-    const yScale = scaleLinear()
-      .domain(yDomain)
-      .range([height - X_AXIS_HEIGHT - CANVAS_BUFFER, CANVAS_BUFFER])
-      .nice();
-    yScale.clamp();
-    const yTicks = yScale.ticks(4);
-
-    return { xScale, xTicks, yScale, yTicks };
-  }, [width, height, xDomain, yDomain]);
-}
-
 export default function SerializationBenchmark() {
-  const theme = useTheme();
   const classes = useStyles();
-  const [ref, { height, width }] = useMeasure();
-
-  // TODO this hook should handle scales and just take data as input
-  const [{ xDomain, yDomain }, setDomains] = useRafState({
-    xDomain: [],
-    yDomain: [],
-  });
   const results = useRef([]);
+  const theme = useTheme();
   const startTime = useRef(window.performance.now());
-
-  const calculateDomains = useCallback(() => {
-    let xDomainMin = Number.MAX_SAFE_INTEGER;
-    let xDomainMax = Number.MIN_SAFE_INTEGER;
-    let yDomainMin = Number.MAX_SAFE_INTEGER;
-    let yDomainMax = Number.MIN_SAFE_INTEGER;
-
-    results.current.forEach(({ timestamp, duration }) => {
-      if (timestamp < xDomainMin) {
-        xDomainMin = timestamp;
-      } else if (timestamp > xDomainMax) {
-        xDomainMax = timestamp;
-      }
-      if (duration < yDomainMin) {
-        yDomainMin = duration;
-      } else if (duration > yDomainMax) {
-        yDomainMax = duration;
-      }
-    });
-
-    setDomains({
-      xDomain: [xDomainMin, xDomainMax],
-      yDomain: [yDomainMin, yDomainMax],
-    });
-  }, [setDomains]);
-
-  const afterBenchmark = useCallback(
-    (timings, name) => {
-      timings.forEach((t) =>
-        results.current.push({
-          duration: t[1],
-          timestamp: t[0],
-          name,
-        })
-      );
-      if (
-        results.current.length % 50 === 0 ||
-        results.current.length === TOTAL_SAMPLES
-      ) {
-        calculateDomains();
-      }
-    },
-    [calculateDomains]
-  );
+  const [_, setLastResult] = useState();
+  const afterBenchmark = useCallback((timings, name) => {
+    timings.forEach((t) =>
+      results.current.push({
+        x: t[0], // time since origin (page load, not 1970)
+        y: t[1], // duration
+        name,
+      })
+    );
+    if (
+      results.current.length % 50 === 0 ||
+      results.current.length === TOTAL_SAMPLES
+    ) {
+      setLastResult(window.performance.now());
+    }
+  }, []);
 
   useEffect(() => {
     const smallerMessage = createComplexNewMessage(300);
@@ -191,106 +126,20 @@ export default function SerializationBenchmark() {
     }
   }, [afterBenchmark]);
 
-  const { xScale, xTicks, yScale, yTicks } = useScales({
-    width,
-    height,
-    xDomain,
-    yDomain,
-  });
+  const pointColors = useMemo(() => {
+    return SERIES_KEYS.reduce((acc, cur, idx) => {
+      acc[cur] = theme.palette.series[idx % theme.palette.series.length];
+      return acc;
+    }, {});
+  }, [theme]);
 
   return (
     <Box>
-      <Box
-        ref={ref}
-        display="flex"
-        flexDirection="column"
-        flex="1 1 100%"
-        height={480}
-        width="100%"
-      >
-        <svg width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
-          <g data-x-axis>
-            <rect
-              className={classes.axisBackground}
-              x={0}
-              y={height - X_AXIS_HEIGHT}
-              width={width}
-              height={X_AXIS_HEIGHT}
-              fill={theme.palette.primary.main}
-              fillOpacity={0.05}
-            />
-            {xTicks
-              .filter((t) => xScale(t) > Y_AXIS_WIDTH * 1.25)
-              .map((t, idx) => (
-                <React.Fragment key={`x-tick-${idx}`}>
-                  <line
-                    key={t}
-                    x1={xScale(t)}
-                    x2={xScale(t)}
-                    y1={height - X_AXIS_HEIGHT}
-                    y2={height}
-                    stroke={theme.palette.primary.main}
-                    strokeWidth={2}
-                  />
-                  <text
-                    className={classes.axisTickText}
-                    x={xScale(t) + 4}
-                    y={height - X_AXIS_HEIGHT + 16}
-                  >
-                    {new Date(
-                      window.performance.timeOrigin + t
-                    ).toLocaleTimeString()}
-                  </text>
-                </React.Fragment>
-              ))}
-          </g>
-          <g data-y-axis>
-            <rect
-              className={classes.axisBackground}
-              x={0}
-              y={0}
-              width={Y_AXIS_WIDTH}
-              height={height - X_AXIS_HEIGHT}
-            />
-            {yTicks
-              .filter((t) => yScale(t) > X_AXIS_HEIGHT * 1.25)
-              .map((t, idx) => (
-                <React.Fragment key={`y-tick-${idx}`}>
-                  <line
-                    key={t}
-                    y1={yScale(t)}
-                    y2={yScale(t)}
-                    x1={0}
-                    x2={Y_AXIS_WIDTH}
-                    stroke={theme.palette.primary.main}
-                    strokeWidth={2}
-                  />
-
-                  <text
-                    className={classes.axisTickText}
-                    x={2}
-                    y={yScale(t) - 4}
-                    textAnchor="start"
-                  >
-                    {t} ms
-                  </text>
-                </React.Fragment>
-              ))}
-          </g>
-          <g data-results>
-            {results.current.map(({ name, timestamp, duration }) => (
-              <circle
-                className={classes[name]}
-                key={`result-${name}-${timestamp}`}
-                r={2.5}
-                fill={theme.palette.series[SERIES_KEYS.indexOf(name)]}
-                cx={xScale(timestamp)}
-                cy={yScale(duration)}
-              />
-            ))}
-          </g>
-        </svg>
-      </Box>
+      <ScatterPlot
+        points={results.current}
+        pointColors={pointColors}
+        pointTitles={{}}
+      />
       <Box mt={2} mb={2}>
         <Typography className={classes.sampleCountText} component="div">
           {results.current.length.toLocaleString()} samples{" "}
@@ -310,18 +159,4 @@ export default function SerializationBenchmark() {
       </Box>
     </Box>
   );
-}
-
-function formatTime(millis) {
-  if (millis > 90 * 1000) {
-    // 90 seconds or longer
-    const mins = Math.round(millis / 1000 / 60);
-    const sec = Math.round((millis / 1000) % 60);
-    return `${mins}m ${sec}s`;
-  }
-  if (millis > 4 * 1000) {
-    // 4 seconds or longer
-    return `${Math.round(millis / 1000)}s`;
-  }
-  return `${Math.round(millis)}ms`;
 }
