5dd9fb36c30c2a7e92db594aa542f132f92453e6
Ted Pennings <310323+tedpennings@users.noreply.github.com>
2021-05-15 22:24:41 -0700

    Eject finally

---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+const path = require("path");
+
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isEnvProduction = process.env.NODE_ENV === "production";
+
+module.exports = {
+  entry: "./src/index.jsx",
+
+  mode: isEnvProduction ? "production" : "development",
+  devtool: isEnvProduction ? false : "cheap-module-source-map",
+
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "build"),
+
+    chunkFilename: isEnvProduction
+      ? "static/js/[name].[contenthash:8].chunk.js"
+      : "static/js/[name].chunk.js",
+
+    publicPath: "public",
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx", ".mdx", ".json"],
+
+    alias: {
+      "react-dom$": "react-dom/profiling",
+      "scheduler/tracing": "scheduler/tracing-profiling",
+    },
+  },
+
+  optimization: {
+    minimize: isEnvProduction,
+    minimizer: [new TerserPlugin()],
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, "build"),
+    compress: true,
+    port: 3000,
+    hot: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Ted Pennings",
+      meta: {
+        viewport: "minimum-scale=1, initial-scale=1, width=device-width",
+      },
+    }),
+  ],
+
+  module: {
+    rules: [
+      // "oneOf" will traverse all following loaders until one will
+      // match the requirements. When no loader matches it will fall
+      // back to the "file" loader at the end of the loader list.
+      {
+        oneOf: [
+          {
+            test: /\.(jpe?g|png|webp)$/i,
+            loader: require.resolve("responsive-loader"),
+            options: {
+              adapter: require("responsive-loader/sharp"),
+              name: "static/media/[name].[hash:8].[ext]",
+              sizes: [1024, 99999],
+            },
+          },
+          {
+            test: /\.(jsx?)$/,
+            loader: require.resolve("babel-loader"),
+          },
+          { test: /.mdx$/, use: ["babel-loader", "@mdx-js/loader"] },
+          {
+            loader: require.resolve("file-loader"),
+            exclude: [/\.(jsx?)$/, /\.html$/, /\.json$/],
+            options: {
+              name: "static/media/[name].[hash:8].[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
