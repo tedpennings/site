1b8303fc307c82bf8cfe7c1447fd9fa5a03fd327
Ted Pennings <310323+tedpennings@users.noreply.github.com>
2021-05-29 01:40:43 -0700

    Add footer

--- src/app.jsx
+++ src/app.jsx
@@ -1,64 +1,125 @@
-import React from "react";
-import { Box, Fade } from "@material-ui/core";
-
+import React, { useState } from "react";
+import { Fade } from "@material-ui/core";
+import { Code, GitHub, LinkedIn } from "@material-ui/icons";
 import { makeStyles } from "@material-ui/core/styles";
 
 import me2021 from "./assets/me-2021.jpg";
 import Heading from "./header"; // TODO rename
 import Landing from "./landing";
+import ConsolidatedLink from "./common/consolidated_link";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: theme.spacing(3, 3, 2, 3),
+  container: {
+    height: "100vh",
+    width: "100vw",
     display: "flex",
     flex: "1 1 100%",
     alignItems: "center",
     justifyContent: "center",
-    height: "100vh",
-    width: "100vw",
+  },
+  root: {
+    padding: theme.spacing(3, 3, 2, 3),
+    display: "grid",
+    gridTemplateColumns: "7fr 5fr",
+    width: "calc(min(100%, 1280px))",
   },
   heroOuter: { background: "#07345F", padding: theme.spacing(2) },
   heroInner: { background: "#D7FBED", padding: theme.spacing(2) },
-  heroImage: { height: "calc(min(60vh, 450px))", width: "auto" },
-  content: {
+  heroImage: { objectFit: "cover", width: "100%" },
+  main: {
     display: "flex",
     flexDirection: "column",
     margin: theme.spacing(0, 2),
     padding: theme.spacing(2),
-    width: "calc(min(45vw, 500px))",
   },
   heading: {
     background: "#07345F",
-    marginLeft: theme.spacing(-4),
-    marginTop: theme.spacing(-4),
-    padding: theme.spacing(3, 2, 2, 4),
-    width: "100%",
+    position: "relative",
+    left: theme.spacing(-4),
+    width: `calc(100% + ${theme.spacing(4)}px)`,
+    marginTop: "15%",
     marginBottom: theme.spacing(4),
+    padding: theme.spacing(3, 2, 2, 4),
+  },
+  content: {
+    display: "flex",
+    flexDirection: "column",
+    flex: "1 1 100%",
+    paddingRight: theme.spacing(2),
+  },
+  footer: {
+    marginTop: "auto",
+    opacity: 0.4,
+    transition: "opacity 300ms",
+    "&:hover": {
+      opacity: 1,
+    },
   },
 }));
 
 export default function App() {
+  const [ready, setReady] = useState(false);
   const classes = useStyles();
   return (
-    // Fade allows time for font parsing, etc
-    <Fade in timeout={{ enter: 500 }}>
-      <Box className={classes.root} data-test-id="root">
-        <Box className={classes.heroOuter}>
-          <Box className={classes.heroInner}>
-            <img
-              className={classes.heroImage}
-              src={me2021}
-              alt="Ted in front of a colorful mural"
-            />
-          </Box>
-        </Box>
-        <Box className={classes.content}>
-          <Box className={classes.heading}>
-            <Heading />
-          </Box>
-          <Landing />
-        </Box>
-      </Box>
+    <Fade in={ready} timeout={1000}>
+      <div className={classes.container}>
+        <div className={classes.root} data-test-id="root">
+          <div className={classes.heroOuter}>
+            <div className={classes.heroInner}>
+              <img
+                className={classes.heroImage}
+                src={me2021}
+                alt="Ted in front of a colorful mural"
+                onLoad={() => {
+                  setReady(true);
+                }}
+              />
+            </div>
+          </div>
+          <div className={classes.main}>
+            <div className={classes.heading}>{ready && <Heading />}</div>
+            <div className={classes.content}>
+              <div>
+                <Landing />
+              </div>
+              <div className={classes.footer}>
+                <Footer />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Fade>
+  );
+}
+
+const useFooterStyles = makeStyles((theme) => ({
+  link: {
+    marginRight: theme.spacing(1),
+  },
+}));
+function Footer() {
+  const classes = useFooterStyles();
+  return (
+    <div>
+      <ConsolidatedLink
+        className={classes.link}
+        href="https://github.com/tedpennings/site"
+      >
+        <Code alt="Code for this website" />
+      </ConsolidatedLink>
+      <ConsolidatedLink
+        className={classes.link}
+        href="https://github.com/tedpennings"
+      >
+        <GitHub alt="My GitHub" />
+      </ConsolidatedLink>
+      <ConsolidatedLink
+        className={classes.link}
+        href="https://www.linkedin.com/in/tedpennings/"
+      >
+        <LinkedIn alt="My LinkedIn" />
+      </ConsolidatedLink>
+    </div>
   );
 }
